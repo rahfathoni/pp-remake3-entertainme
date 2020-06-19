64c702228842825e450a0232d978d0cdd0566277
rahfathoni <rahfathoni@gmail.com>
2020-06-19 14:11:51 +0700

    initial file for microservices

--- orchestrator/app.js
+++ orchestrator/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors')
 
 const { connect } = require('./config/mongo')
 connect((err) => {
     if(!err){
+        app.use(cors())
         app.use(express.urlencoded({extended: false}));
         app.use(express.json());
         app.use('/', require('./routes/index'))
